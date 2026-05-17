cd814f14ac30f1a93e4c1b90e7c41b52027e7215
toreken <natsushimo2224@gmail.com>
2026-05-18 00:02:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -142,8 +142,9 @@ def index():
     with open('index.html', 'r', encoding='utf-8') as f:
         return f.read()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
